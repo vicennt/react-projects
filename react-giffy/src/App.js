d2a292a164d355a5af00a1a2252bd3905e3354cd
Vicent Perez <vicennt@gmail.com>
2020-04-17 18:39:59 +0200

    new react project: giffy

---
+++ react-giffy/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="App-content">
+        gifs
+      </section>
+    </div>
+  );
+}
+
+export default App;
