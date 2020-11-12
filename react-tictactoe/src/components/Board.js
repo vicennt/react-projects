4c7e7f590b304d52429b921939644b7703cb4726
Vicent Pérez <vicennt@gmail.com>
2020-11-12 15:30:58 +0100

    [EXTRA] Board created using loops

--- react-tictactoe/src/components/Board.js
+++ react-tictactoe/src/components/Board.js
@@ -12,24 +12,20 @@ export default class Board extends React.Component {
   }
 
   render() {
+    let tableSize = 3;
+    let cont = 0;
+    let row = [];
+    let board = [];
+    for(let i = 0; i < tableSize; i++){
+      for (let j = 0; j < tableSize; j++){
+          row.push(this.renderSquare(cont))
+          cont++;
+      }     
+      board.push(<div className="board-row">{row}</div>);
+      row = [];
+    }
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div> {board} </div>
     );
   }
 }
