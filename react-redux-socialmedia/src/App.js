aa54535be5cc761a7a39678311e5c25ebb4f113b
Vicent Pérez <vicennt@gmail.com>
2020-11-18 15:44:08 +0100

    reading a particular post in a single page

--- react-redux-socialmedia/src/App.js
+++ react-redux-socialmedia/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from 'react-router-dom'
+} from 'react-router-dom';
 
-import { Navbar } from './app/Navbar'
-import { PostsList} from './features/posts/PostsList'
-import { AddPostForm} from './features/posts/AddPostForm'
+import { Navbar } from './app/Navbar';
+import { PostsList } from './features/posts/PostsList';
+import { AddPostForm } from './features/posts/AddPostForm';
+import { SinglePostPage } from './features/posts/SinglePostPage';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
               </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage}/>
           <Redirect to="/" />
         </Switch>
       </div>
