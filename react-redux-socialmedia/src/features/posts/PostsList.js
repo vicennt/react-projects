14b2188fe73b20521cb26b60ac611e2686ad5d14
Vicent Pérez <vicennt@gmail.com>
2020-11-19 11:38:00 +0100

    displaying loading state

--- react-redux-socialmedia/src/features/posts/PostsList.js
+++ react-redux-socialmedia/src/features/posts/PostsList.js
@@ -9,7 +9,8 @@ import { selectAllPosts, fetchPosts } from './postsSlice';
 export const PostsList = () => {
     const dispatch = useDispatch();
     const posts = useSelector(selectAllPosts);
-    const postsStatus = useSelector(state => state.posts.status)
+    const postsStatus = useSelector(state => state.posts.status);
+    const error = useSelector(state => state.posts.error);
 
     useEffect(() => {
         if(postsStatus === 'idle') {
@@ -17,24 +18,30 @@ export const PostsList = () => {
         }
     }, [postsStatus, dispatch]);
 
-    
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    let content;
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article className="post-excerpt" key={post.id}>
-            <h3>{post.title}</h3>
-            <PostAuthor userId={post.userId}/>
-            <TimeAgo timestamp={post.date}/>
-            <p className="post-content">{post.content.substring(0, 100)}</p>
-            <ReactionButtons post={post}/>
-            <Link to={`/posts/${post.id}`} className="button muted-button">View post</Link>
-        </article>
-    ))
+    if(postsStatus === "loading") {
+        content = <div className="loader">Loading...</div>;
+    }else if (postsStatus === "succeeded") {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => (
+            <article className="post-excerpt" key={post.id}>
+                <h3>{post.title}</h3>
+                <PostAuthor userId={post.userId}/>
+                <TimeAgo timestamp={post.date}/>
+                <p className="post-content">{post.content.substring(0, 100)}</p>
+                <ReactionButtons post={post}/>
+                <Link to={`/posts/${post.id}`} className="button muted-button">View post</Link>
+            </article>
+        ))
+    } else if (postsStatus === "failed") {
+        content = <div>{error}</div>
+    }
 
     return (
         <section className="posts-list">
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     )
 }
