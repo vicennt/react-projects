a049043c3ec2fc696bed59bfcbffb6217e4d60aa
Vicent Pérez <vicennt@gmail.com>
2020-11-19 14:56:33 +0100

    created a notification slice

--- react-redux-socialmedia/src/app/store.js
+++ react-redux-socialmedia/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice';
 import usersReducer from '../features/users/usersSlice';
+import notificationReducer from '../features/notifications/notificationsSlice';
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
-    users: usersReducer
+    users: usersReducer,
+    notifications: notificationReducer
   }
 })
