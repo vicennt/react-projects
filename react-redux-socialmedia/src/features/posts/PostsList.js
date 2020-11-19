db4cf92fe26ba2618d3381abf496b7fd7f1e152f
Vicent Pérez <vicennt@gmail.com>
2020-11-19 11:02:08 +0100

    dispatching thunks from components

--- react-redux-socialmedia/src/features/posts/PostsList.js
+++ react-redux-socialmedia/src/features/posts/PostsList.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { PostAuthor } from './PostAuthor';
 import { TimeAgo } from './TimeAgo';
 import { ReactionButtons } from './ReactionButtons';
-import { selectAllPosts } from './postsSlice';
+import { selectAllPosts, fetchPosts } from './postsSlice';
 
 export const PostsList = () => {
+    const dispatch = useDispatch();
     const posts = useSelector(selectAllPosts);
+    const postsStatus = useSelector(state => state.posts.status)
+
+    useEffect(() => {
+        if(postsStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postsStatus, dispatch]);
+
+    
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
     const renderedPosts = orderedPosts.map(post => (
--- react-redux-socialmedia/src/features/posts/postsSlice.js
+++ react-redux-socialmedia/src/features/posts/postsSlice.js
@@ -1,10 +1,16 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+import { client } from '../../api/client'; 
 
 const initialState = {
     posts : [],
     status: "idle",
     error: null
 }
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+    const response = await client.get('/fakeApi/posts');
+    return response.posts;
+})
 
 const postsSlice = createSlice({
     name: 'posts',
