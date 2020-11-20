27d1a58860a910c3b12f4e448505aaa43b186e13
Vicent Pérez <vicennt@gmail.com>
2020-11-20 15:08:43 +0100

    selecting data using todos id instead of the entire todo object

--- react-redux-todolist/src/features/todos/TodoList.js
+++ react-redux-todolist/src/features/todos/TodoList.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import TodoListItem from './TodoListItem'
 
-// Funcion selectora (selecciona el array de todos dentro del estado)
-const selectTodos = state => state.todos;
+const selectTodoIds = state => state.todos.map(todo => todo.id);
 
 const TodoList = () => {
-  const todos = useSelector(selectTodos) // useSelector se subscribe automaticamente a la store
-  // Cada vez que una action sea disparada se ejecuta la funcion selectora automaticamente
-  const renderedListItems = todos.map((todo) => {
-    return <TodoListItem key={todo.id} todo={todo} />
+  const todoIds = useSelector(selectTodoIds, shallowEqual);
+  const renderedListItems = todoIds.map((todoId) => {
+    return <TodoListItem key={todoId} id={todoId} />
   })
-
   return <ul className="todo-list">{renderedListItems}</ul>
 }
 
