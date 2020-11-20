e03c17849fe9a8be5f6959fb84ce2cee1190c71a
Vicent Pérez <vicennt@gmail.com>
2020-11-20 14:24:13 +0100

    getting todos from store using useSelector hook

--- react-redux-todolist/src/features/todos/TodoList.js
+++ react-redux-todolist/src/features/todos/TodoList.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 import TodoListItem from './TodoListItem'
 
-const TodoList = () => {
-  const todos = []
+// Funcion selectora (selecciona el array de todos dentro del estado)
+const selectTodos = state => state.todos;
 
+const TodoList = () => {
+  const todos = useSelector(selectTodos) // useSelector se subscribe automaticamente a la store
+  // Cada vez que una action sea disparada se ejecuta la funcion selectora automaticamente
   const renderedListItems = todos.map((todo) => {
     return <TodoListItem key={todo.id} todo={todo} />
   })
