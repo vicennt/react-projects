f065f0b11b99acdde7d4ed138ead8615bd5be39e
Vicent Pérez <vicennt@gmail.com>
2020-11-23 09:41:47 +0100

    mark all completed & clear completed implemented

--- react-redux-todolist/src/features/footer/Footer.js
+++ react-redux-todolist/src/features/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import RemainingTodos from './RemainingTodos';
 import StatusFilter from './StatusFilter';
@@ -7,6 +7,8 @@ import ColorFilters from './ColorsFilter';
 
 
 const Footer = () => {
+
+  const dispatch = useDispatch()
 
   const todosRemaining = useSelector(state => {
     const uncompletedTodos = state.todos.filter(todo => !todo.completed);
@@ -16,13 +18,15 @@ const Footer = () => {
   const { status, colors } = useSelector(state => state.filter);
   const onColorChange = (color, changeType) => console.log('Color change: ', { color, changeType });
   const onStatusChange = (status) => console.log('Status change: ', status);
+  const onMarkAllCompletedClicked = () => dispatch({ type : 'todos/allCompleted'});
+  const onClearCompletedClicked = () => dispatch({ type: 'todos/completedCleared'});
 
   return (
     <footer className="footer">
       <div className="actions">
         <h5>Actions</h5>
-        <button className="button">Mark All Completed</button>
-        <button className="button">Clear Completed</button>
+        <button className="button" onClick={onMarkAllCompletedClicked}>Mark All Completed</button>
+        <button className="button" onClick={onClearCompletedClicked}>Clear Completed</button>
       </div>
 
       <RemainingTodos count={todosRemaining} />
